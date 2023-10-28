0e26c1d8a7bb3abe283a2840a67b1a2148315987
John Brunton <john_brunton@live.co.uk>
2023-10-28 13:58:45 +0100

    refactor: move CycleTimesUseCase

--- api/src/app/datasets/datasets.module.ts
+++ api/src/app/datasets/datasets.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { DatasetsController } from "./datasets.controller";
 import { SyncUseCase } from "@usecases/datasets/sync/sync-use-case";
-import { CycleTimesUseCase } from "@usecases/cycle-times-use-case";
+import { CycleTimesUseCase } from "@usecases/issues/metrics/cycle-times-use-case";
 
 @Module({
   providers: [SyncUseCase, CycleTimesUseCase],
