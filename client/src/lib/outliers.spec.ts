e0e61596f778f5a82540a89c37c8e1335235760d
John Brunton <john_brunton@live.co.uk>
2023-12-11 22:15:28 +0000

    chore: tests for outliers

---
+++ client/src/lib/outliers.spec.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from "vitest";
+import { excludeOutliersFromSeq } from "./outliers";
+
+describe("excludeOutliersFromSeq", () => {
+  const seqWithoutOutliers = [{ x: 1 }, { x: 2 }, { x: 1 }, { x: 3 }];
+
+  it("filters out outliers if they exist", () => {
+    const result = excludeOutliersFromSeq(
+      [...seqWithoutOutliers, { x: 100 }],
+      (a) => a.x,
+    );
+    expect(result).toEqual(seqWithoutOutliers);
+  });
+
+  it("filters nothing if there are no outliers", () => {
+    const result = excludeOutliersFromSeq(seqWithoutOutliers, (a) => a.x);
+    expect(result).toEqual(seqWithoutOutliers);
+  });
+});
